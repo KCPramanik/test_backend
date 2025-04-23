b379a3a1176af93f284d1aca8e737d47e0c6a194
kartik chandra pramanik <kartikchandrapramanik01@gmail.com>
2025-04-23 17:27:23 +0530

    update get method and add post method

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/login', (req, res) => {
+  let dateTime = new Date()
+  let date = dateTime.toLocaleDateString()
+  let time = dateTime.toLocaleTimeString()
+  res.send(`The current date is ${date} and the current time is ${time}`)
 
-app.get('/login',(req,res) => {
-    res.send('Login Page')
+})
+
+app.post('/api/data', (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  res.send(`Hello  ${name}, your age is : ${age}`);
+  
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on localhost ${port}`)
 })
